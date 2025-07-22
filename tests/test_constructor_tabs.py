4451d6ec1721beb0fec0bbdac27e51bf1aff37e2
Serik Kumarbek <s.kumarbek@mail.ru>
2025-07-22 17:46:51 +0500

    новый тест на конструктор

--- tests/test_constructor_tabs.py
+++ tests/test_constructor_tabs.py
@@ -1,22 +1,29 @@
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from locators import ConstructorPageLocators
 
-def wait_for_active_tab(driver, expected_text, timeout=5):
+
+def click_and_wait_for_tab(driver, tab_locator, expected_text, timeout=5):
+    WebDriverWait(driver, timeout).until(
+        EC.element_to_be_clickable(tab_locator)
+    ).click()
+
     WebDriverWait(driver, timeout).until(
         lambda d: d.find_element(*ConstructorPageLocators.ACTIVE_TAB).text == expected_text
     )
 
+    active_tab = driver.find_element(*ConstructorPageLocators.ACTIVE_TAB).text
+    assert active_tab == expected_text, f"Ожидалась активная вкладка '{expected_text}', но была '{active_tab}'"
+
+
 def test_constructor_tabs(driver):
     driver.get("https://stellarburgers.nomoreparties.site/")
 
-    # Клик на "Соусы" и ожидание
-    driver.find_element(*ConstructorPageLocators.SAUCES_TAB).click()
-    wait_for_active_tab(driver, "Соусы")
+    # Переключаемся на "Соусы"
+    click_and_wait_for_tab(driver, ConstructorPageLocators.SAUCES_TAB, "Соусы")
 
-    # Клик на "Начинки" и ожидание
-    driver.find_element(*ConstructorPageLocators.FILLINGS_TAB).click()
-    wait_for_active_tab(driver, "Начинки")
+    # Переключаемся на "Начинки"
+    click_and_wait_for_tab(driver, ConstructorPageLocators.FILLINGS_TAB, "Начинки")
 
-    # Клик на "Булки" и ожидание
-    driver.find_element(*ConstructorPageLocators.BUNS_TAB).click()
-    wait_for_active_tab(driver, "Булки")
+    # Переключаемся на "Булки"
+    click_and_wait_for_tab(driver, ConstructorPageLocators.BUNS_TAB, "Булки")
