269cef4091c563f0393abbbc97850a92c3878859
Serik Kumarbek <s.kumarbek@mail.ru>
2025-07-22 19:36:42 +0500

    теперь точно енд

--- tests/test_constructor_tabs.py
+++ tests/test_constructor_tabs.py
@@ -2,7 +2,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from locators import ConstructorPageLocators
 
-
+# Функция для клика по вкладке и ожидания её активации
 def click_and_wait_for_tab(driver, tab_locator, expected_text, timeout=5):
     tab = WebDriverWait(driver, timeout).until(
         EC.element_to_be_clickable(tab_locator)
@@ -17,7 +17,7 @@ def click_and_wait_for_tab(driver, tab_locator, expected_text, timeout=5):
     active_tab = driver.find_element(*ConstructorPageLocators.ACTIVE_TAB).text.strip()
     assert active_tab == expected_text, f"Ожидалась активная вкладка '{expected_text}', но была '{active_tab}'"
 
-
+# Тест для проверки переключения между вкладками в конструкторе бургеров
 def test_constructor_tabs(driver):
     driver.get("https://stellarburgers.nomoreparties.site/")
 
